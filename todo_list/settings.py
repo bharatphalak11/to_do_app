95db2b951d25873d077c5ca6ac2b1c66b552c984
bharatphalak11 <bharat.phalak@aceerpsolutions.com>
2023-08-19 17:00:26 +0530

    Changes in static files and UI

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -128,9 +128,9 @@ LOGIN_URL = 'login'
 #STATIC_ROOT = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'    
+STATIC_URL = '/staticfiles/'    
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
